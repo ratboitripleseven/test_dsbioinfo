1845a8e0dda9f9f9d8fd28c96cf3bba89833819d
ratboitripleseven <saw7x@hotmail.com>
2022-10-30 11:37:25 +0100

    "added name subplot"

--- plot.py
+++ plot.py
@@ -6,8 +6,11 @@ values_y_1 = [random.randint(1,10) for i in range(0,10)]
 values_x_2 = [0,1,2,3,4,5,6,7,8,9]
 values_y_2 = [random.randint(1,10) for i in range(0,10)]
 
+
+fig = plt.figure()
 plt.scatter(values_x_1, values_y_1)
 plt.plot(values_x_2, values_y_2)
+fig.suptitle('Random Plot', fontsize=8)
 plt.savefig("test.png")
 plt.show()
 
